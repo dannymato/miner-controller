0ac44b810a8216c9048b574bf433693038572104
Daniel Mato <danny.mato1@gmail.com>
2021-05-01 23:47:12 -0400

    Adds in GPU methods

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
 import express from 'express';
-import {createMiner} from './miner';
+import createMiner from './miner';
 const app = express();
 const port = 3000;
 
+// TODO: Load this from a config file or something like that
 const miner = createMiner('localhost', 4028);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.get('/summary', (req, res) => {
   miner.summary((data) => {
+    res.send(data);
+  });
+});
+
+app.get('/gpus/count', (req, res) => {
+  miner.gpuCount((data) => {
+    res.send(data);
+  });
+});
+
+app.put('/gpus/:index/enable', (req, res) => {
+  miner.enableGpu(req.params['index'], (data) => {
+    res.send(data);
+  });
+});
+
+app.put('/gpus/:index/disable', (req, res) => {
+  miner.disableGpu(req.params['index'], (data) => {
+    res.send(data);
+  });
+});
+
+app.get('/gpus/:index', (req, res) => {
+  miner.gpu(req.params['index'], (data) => {
     res.send(data);
   });
 });
--- src/miner.ts
+++ src/miner.ts
@@ -1,20 +1,20 @@
 import MinerAPI from './api';
 
-export interface MinerReturn<ItemType extends Item> {
+interface MinerReturn<ItemType extends Item> {
   status: Status;
   data: ItemType;
 }
 
 interface Item {}
 
-export interface Summary extends Item {
+interface Summary extends Item {
   Elapsed: number;
   'Found Blocks': number;
   'MHS av': number;
   'MHS 30s': number;
 }
 
-export interface Status {
+interface Status {
   STATUS: string;
   When: number;
   Code: number;
@@ -22,11 +22,11 @@ export interface Status {
   Description: string;
 }
 
-export interface GPUs extends Item {
-  GPU: GPU[];
+interface GPUs extends Item {
+  GPU: GPU;
 }
 
-export interface GPU {
+interface GPU {
   GPU: number;
   Enabled: string;
   Temperature: number;
@@ -42,12 +42,19 @@ export interface GPU {
   Rejected: number;
 }
 
-type SummaryCallback = (data: MinerReturn<Summary>) => void
+interface Count {
+  Count: number;
+}
+
+type SummaryCallback = (data: MinerReturn<Summary>) => void;
+type GpusCallback = (data: MinerReturn<GPUs>) => void;
+type CountCallback = (data: MinerReturn<Count>) => void;
+type StatusCallback = (data: Status) => void;
 
 /**
  * Class contains methods to retrieve items from the miner
  */
-export class Miner {
+class Miner {
   /**
    * This one may not really need the host and port might delete
    * @param {string} host host that points to the miner
@@ -69,8 +76,89 @@ export class Miner {
     });
   }
 
+  // Not an actual thing in the api
+  // Could be changed to devices instead
+  // /**
+  //  * Grabs the info on all GPUs
+  //  * @param {GpusCallback} callback What to do with the parsed data
+  //  */
+  // public gpus(callback: GpusCallback) {
+  //   const command = 'gpu';
+  //   const params = '';
+
+  //   this.api.sendCommand(command, params, (data) => {
+  //     callback(this.parseMessage<GPUs>(data.toString(), 'GPU'));
+  //   });
+  // }
+
+  /**
+   * Grabs info about the specified GPU
+   * @param {string} index The GPU index to grab
+   * @param {GpusCallback} callback What to do with the parsed data
+   */
+  public gpu(index: string, callback: GpusCallback) {
+    const command = 'gpu';
+    const params= index;
+
+    this.api.sendCommand(command, params, (data) => {
+      callback(this.parseMessage<GPUs>(data.toString(), 'GPU'));
+    });
+  }
+
+  /**
+   *
+   * @param {CountCallback} callback
+   */
+  public gpuCount(callback: CountCallback) {
+    const command = 'gpucount';
+    const params = '';
+
+    this.api.sendCommand(command, params, (data) => {
+      callback(this.parseMessage(data.toString(), 'GPUS'));
+    });
+  }
+
+  /**
+   * Enable the specified GPU
+   * @param {string} index The index of the gpu to enable
+   * @param {StatusCallback} callback What to do with the data
+   */
+  public enableGpu(index: string, callback: StatusCallback) {
+    const command = 'gpuenable';
+    const params = index;
+
+    this.api.sendCommand(command, params, (data) => {
+      callback(this.statusOnlyParseMessage(data.toString()));
+    });
+  }
+
+  /**
+   * Disable the specified GPU
+   * @param {string} index The index of the gpu to disable
+   * @param {StatusCallback} callback What to do with the data
+   */
+  public disableGpu(index: string, callback: StatusCallback) {
+    const command = 'gpudisable';
+    const params = index;
+
+    this.api.sendCommand(command, params, (data) => {
+      callback(this.statusOnlyParseMessage(data.toString()));
+    });
+  }
+
+  /**
+   * Converts the calls which only return a status
+   * @param {string} message The message from the API
+   * @return {Status} The status from the API
+   */
+  private statusOnlyParseMessage(message: string): Status {
+    const obj = JSON.parse(message);
+    return obj.STATUS[0];
+  }
+
   /**
    * Converts the raw input into nice TS types
+   * Should eventually be a kind of conversion to nicer objects
    * @param {string} message The message from the API
    * @param {string} parameterName The name of the parameter
    * I don't like parameterName should be removed
@@ -80,17 +168,18 @@ export class Miner {
       parameterName: string): MinerReturn<MessageType> {
     const obj = JSON.parse(message);
     return {
-      status: obj.STATUS,
-      data: obj[parameterName],
+      status: obj.STATUS[0],
+      data: obj[parameterName][0],
     };
   }
 }
+
 /**
  * @param {string} host host that points to the miner
  * @param {number} port port of the MinerAPI
  * @return {Miner} returns a newly created miner
  */
-export function createMiner(host: string, port: number) : Miner {
+export default function createMiner(host: string, port: number) : Miner {
   const api = new MinerAPI(host, port);
   return new Miner(host, port, api);
 }
